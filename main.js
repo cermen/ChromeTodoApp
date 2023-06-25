73bac39912fe3c53d4438acf4fe5dd0eb1ec497b
jyu <yjs01016@naver.com>
2023-06-25 16:46:39 +0900

    이름 입력 기능 구현

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", () => {
     setBackground();
     setQuote();
-})
+});
 
 const setBackground = () => {
     const body = document.querySelector("body");
     const imgs = [
         'bangkok.jpg', 'greece.jpg', 'gyeongju.jpg', 'jeju.jpg', 'london.jpg',
-        'osaka.jpg', 'paris.jpg', 'rome.jpeg', 'seoul.jpg', 'venice.jpeg'
+        'newyork.jpg', 'osaka.jpg', 'paris.jpg', 'rome.jpeg', 'seoul.jpg'
     ];
     body.style.backgroundImage = `url("img/bgs/${imgs[Math.floor(Math.random() * imgs.length)]}")`;
 }
@@ -22,7 +22,7 @@ const setQuote = () => {
         {content: "I hated every minute of training, but I said, “Don't quit. Suffer now and live the rest of your life as a champion.”", author: "Muhammad Ali"},
         {content: "성공하려고 아무리 열심히 노력해도 실패에 대한 두려움이 마음에 가득하다면, 노력하지 않게 되고 정진이 허사가 되어 성공은 불가능해질 것이다.", author: "보두앵"},
         {content: "자신을 두렵게 하는 일을 매일 하라.", author: "작자 미상"},
-        {content: "Genius is one percent inspiration and ninety-nine percent perspiration", author: "Thomas Edison"},
+        {content: "Genius is one percent inspiration and ninety-nine percent perspiration.", author: "Thomas Edison"},
         {content: "슬럼프라 정의하는 순간 슬럼프가 된다.", author: "작자 미상"},
         {content: "You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future.", author: "Steve Jobs"},
         {content: "99도까지 열심히 올려놓아도 마지막 1도를 넘기지 못하면 영원히 물은 끓지 않는다. 물을 끓이는 건 마지막 1도, 포기하고 싶은 그 1분을 참아내는 것이다. 이 순간을 넘어야 그 다음 문이 열린다.", author: "김연아"},
@@ -34,6 +34,30 @@ const setQuote = () => {
     quoteAuthor.innerText = `- ${quote[idx].author}`;
 }
 
+const loginName = document.querySelector("#login__name");
+loginName.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        const loginDiv = document.querySelector(".login");
+        const header = document.querySelector("header");
+        const headerGreeting = document.querySelector(".header__greeting");
+        const main = document.querySelector("main");
+
+        localStorage.removeItem("name");
+        localStorage.setItem("name", loginName.value);
+        console.log(localStorage.getItem("name"));
+
+        // 웹페이지 시작시 실행
+        headerGreeting.innerText = `${localStorage.getItem("name")}님 안녕하세요!`;
+        setTime();
+        setDate();
+
+        loginDiv.style.display = "none";
+        header.classList.toggle("none");
+        main.classList.toggle("none");
+    }
+})
+
+// 시각 표기 함수
 const setTime = () => {
     const clockTime = document.querySelector(".clock__time");
 
@@ -46,6 +70,7 @@ const setTime = () => {
     clockTime.innerHTML = `${hour}:${minute}<span id="clock__time__second">${second}</span>`;
 }
 
+// 날짜 표기 함수
 const setDate = () => {
     const clockDate = document.querySelector(".clock__date");
 
@@ -60,9 +85,6 @@ const setDate = () => {
     clockDate.innerHTML = `${year}년 ${month}월 ${date}일 ${day}요일`;
 }
 
-setTime();
-setDate();
-
 setInterval(() => {
     setTime();
 
@@ -72,3 +94,9 @@ setInterval(() => {
     }
 }, 1000);
 
+// const todoList = document.querySelector(".todo__list");
+// const todoAddBtn = document.querySelector(".todo__add__btn");
+// todoAddBtn.addEventListener("click", () => {
+//     const newTodo = document.querySelector(".todo__add__input").value;
+    
+// })
